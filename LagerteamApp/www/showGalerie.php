e43cf91ebf1a44c09adb64ef88c43e631683d30d
Raphael Menken <raphthenuse@googleamil.com>
2018-10-26 09:25:37 +0200

    RM: Erstellung von der Datei ShowGalerie.php
    Die ShowGalerie.php erhält wieder die vordefinierte Navbar.
    Was ändert sich für den Benutzer?
    -Die showGalerie wird beim Aufruf einer Teilgalerie durch einen Button und einem hinterlegten Link gestartet.
    Was wird am Code gemacht?
    -Der Code enthält php-Befehle, in Zeile 1-3 wird die durch die Variable show der Name aus der Url geholt.
    -Zeile 77 gibt Galerie + den Namen des folders also dem Ordner als Überschrift aus
    -im div mit der class "demo" ist eine unordered list in der eine php Abfrage statt findet. Zeile 81: In $directory wird der Dateipfad angegeben, durch den der richtige Ordner ausgewählt wird. Der nächste Befehl in Zeile 82: Mit glob wird der Dateiname gefunden, hier sind es die .jpg Dateien und in $images gespeichert.
    -In der foreach-Schleife werden alle §images ausgwählt und durch echo ausgegeben.

---
+++ LagerteamApp/www/showGalerie.php
@@ -0,0 +1,108 @@
+<?php
+$folder=$_GET["show"];
+ ?>
+
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+
+    <!-- Bootstrap & Default-CSS einbinden -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/default.css" />
+    <link rel="stylesheet" href="css/lightslider.css">
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="js/lightslider.js"></script>
+
+    <!-- Eigene Stylesheets & Scripte-->
+    <link rel="stylesheet" href="css/eigene.css" />
+    <script src="js/eigene.js"></script>
+    <style>
+    .demo {
+        width:50%;
+    }
+    ul {
+        list-style: none outside none;
+        padding-left: 0;
+        margin-bottom:0;
+    }
+    li {
+        display: block;
+        float: left;
+        margin-right: 6px;
+        cursor:pointer;
+    }
+    li > img {
+        display: block;
+        height: auto;
+        width: 100%;
+        object-fit: cover;
+    }
+    </style>
+</head>
+
+<body>
+    <!-- Navigationsleiste (fixed)-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="#">Logo</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="#">Menü  <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="#">Online-Anmeldung</a>
+                <a class="nav-item nav-link" href="#">Kalender</a>
+                <a class="nav-item nav-link" href="#">Galerie</a>
+                <a class="nav-item nav-link" href="#">Songtexte</a>
+                <a class="nav-item nav-link" href="#">Kontakt</a>
+                <a class="nav-item nav-link" href="#">Intern</a>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Seiteninhalt -->
+    <main>
+        <!--
+        Erster Section Abschnitt.
+        Beeinhaltet die Klasse "container-navbar-fixed".
+        Zuständig für den margin-top Abstand von der fixed-Navbar.
+        -->
+        <section class="container container-navbar-fixed">
+            <h1>Galerie <?php echo $folder; ?></h1>
+            <div class="demo">
+              <ul id="lightSlider">
+                <?php
+                $directory = "img/Galerie/" . $folder . "/";
+                $images = glob($directory . "*.jpg");
+
+                foreach ($images as $img) {
+                  echo '<li data-thumb="' . $img . '"> <img src="' . $img . '"></li>';
+                }
+                 ?>
+               </ul>
+             </div>
+        </section>
+    </main>
+
+</body>
+
+<!-- Platz für Skript innerhalb der html-Datei -->
+<script>
+$('#lightSlider').lightSlider({
+      gallery: true,
+      item: 1,
+      loop: true,
+      slideMargin: 0,
+      adaptiveHeight:true,
+      enableTouch: true,
+      thumbItem: 9
+  });
+</script>
+
+</html>
