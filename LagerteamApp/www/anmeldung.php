50da45db6d83f4bf3b4f4919f25649c9c4ca4f84
marceljakob <marcel.jakob@protonmail.com>
2018-11-16 11:23:11 +0100

    MJ: Online-Anmeldung Grundlayout
    mja: Grundformular für die Online-Anmeldung + dazugehöriges CSS File

---
+++ LagerteamApp/www/anmeldung.php
@@ -0,0 +1,56 @@
+<!-- Bei dieser Datei handelt es sich um die Startseite der App. -->
+
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Online-Anmeldung</title>
+
+    <!-- Bootstrap einbinden -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Eigene Stylesheets -->
+    <link rel="stylesheet" href="css/kontaktformular.css">
+    <link rel="stylesheet" href="css/default.css"/>
+
+</head>
+
+<body>
+    <!-- Navigationsleiste -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="./"><img src="img/logo.png"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link" href="./">Menü</a>
+                <a class="nav-item nav-link active" href="anmeldung.html">Online-Anmeldung<span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="kalender.html">Kalender</a>
+                <a class="nav-item nav-link" href="galerie.html">Galerie</a>
+                <a class="nav-item nav-link" href="songtexte.html">Songtexte</a>
+                <a class="nav-item nav-link" href="kontaktformular.php">Kontakt</a>
+                <a class="nav-item nav-link" href="intern.php">Intern</a>
+            </div>
+        </div>
+    </nav>
+
+
+    <!-- Im nachfolgenden main-Bereich wird der Container für das Anmeldeformular definiert -->
+    <main>
+        <!-- Container für das Anmeldeformular -->
+
+
+    </main>
+
+
+
+</body>
+
+</html>
