5a5b255606ceddc8c75e959f3b59d45f8e1e6ef3
marceljakob <marcel.jakob@protonmail.com>
2018-11-18 12:25:30 +0100

    MJ: Auto Email nach Anmeldung
    Die automatische Emailfunktion bei erfolgreicher Anmeldung wurde ergänzt und getestet + Fehlerprüfung

--- LagerteamApp/www/anmeldung.php
+++ LagerteamApp/www/anmeldung.php
@@ -77,10 +77,60 @@
         echo "</br>";
         echo "Hier gehts zurück zur  <a href='./'>Startseite</a>!";
         }
-
-
-
         ?>
+
+        <?php
+        include 'sendMail.php';
+        $mail->SetFrom("lagerteamapp2018@gmail.com", "Lagerteam");
+        $mail->AddAddress("lagerteamapp2018@gmail.com"); //Empfänger Adresse, Weitere Adressen über $mail->AddAddress hinzufügen oder als CC/BCC
+        //Set an alternative reply-to address
+        //$mail->addReplyTo('replyto@example.com', 'First Last');
+        //Set who the message is to be sent to
+        $mail->addAddress($email);
+        //Set the subject line
+        $mail->Subject = 'Anmeldebestaetigung zum Zeltlager';
+        //Read an HTML message body from an external file, convert referenced images to embedded,
+        //convert HTML into a basic plain-text alternative body
+        //$mail->msgHTML(file_get_contents('test.html'), dirname(/var/www/html/));
+        $mail->IsHTML(true);
+        $mail->Body='Guten Tag,<br />
+        <br />
+        Hiermit m&ouml;chten wir Ihnen den Eingang der Anmeldung Ihres Sohnes zu unserem j&auml;hrlichen Zeltlager best&auml;tigen.<br />
+        Wir bitten Sie den Beitrag von 240,00 Euro auf unser Konto der <b>Spar- und Kreditbank Rheinstetten eG</b> zu &uuml;berweisen.<br />
+        <br />
+        <b>IBAN: DE53 6606 1407 0008 0219 61</b><br />
+        <b>BIC: GENODE61RH2</b><br/>
+        <br />
+        Vermerken Sie bei der &Uuml;berweisung unbedingt den Namen Ihres Kindes!<br />
+        Unsere Finanzw&auml;rte pr&uuml;fen w&ouml;chentlich unser Bankkonto auf Neueing&auml;nge.<br />
+        Sobald Ihre &Uuml;berweisung bei uns eingetroffen ist, erhalten Sie eine Email mit der Zahlungsbest&auml;tigung.<br />
+        <br />
+        <br />
+        Im Anhang befindet sich zudem unser gewohntes Info-Schreiben (Ansprechpartner, Termine, Beantragung von Zuschuss, Reiser&uuml;cktritt, usw.) und der Dokumentenumschlag, den Sie Ihrem Sohn bei der Abfahrt der Busse mitgeben.<br />
+        <br />
+        <br />
+        Viele Gr&uuml;&szlig;e,<br />
+        Ihr Lagerteam<br /><br />
+        <i>i.A. Marcel Jakob<br />
+        marcel@lagerteam.de</i>';
+
+        //$mail->body = 'Hello, this is my message.';
+        //Replace the plain text body with one created manually
+        $mail->AltBody = '';
+        //Attach an image file
+        $mail->addAttachment('Anmeldung_2016.pdf');
+        //send the message, check for errors
+        ?>
+
+        <?php
+        echo "</br></br>";
+        if (!$mail->send()) {
+            echo "Es konnte leider keine Email an Sie gesendet werden, bitte wenden Sie sich an marcel@lagerteam.de <br /><br />" . $mail->ErrorInfo;
+        } else {
+            echo "Bitte Überprüfen Sie Ihr Email-Postfach, Sie sollten eine Email über die Eingangsbestätigung der Anmeldung Ihres Sohnes bekommen haben.";
+        }
+        ?>
+
       </main>
 
 
